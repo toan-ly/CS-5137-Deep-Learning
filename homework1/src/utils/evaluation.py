cb4620564da3021bd2c456ab52db8bde4333388b
toan-ly <toanly0521@gmail.com>
2025-09-14 23:14:03 -0400

    fix: failed trying to normalize target label

--- homework1/src/utils/evaluation.py
+++ homework1/src/utils/evaluation.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+import joblib
 
 def plot_density(df, num_cols=4):
     """
@@ -13,15 +14,25 @@ def plot_density(df, num_cols=4):
     axes = axes.flatten()
 
     for i, col in enumerate(num_features):
-        sns.kdeplot(df[col], ax=axes[i])
+        # sns.kdeplot(df[col], ax=axes[i], color='red')
+        sns.histplot(df[col], ax=axes[i], kde=True, stat='density', bins=30, alpha=0.3)
         axes[i].set_title(f'{col}')
 
     plt.tight_layout()
     plt.show()
+
+def load_scaler(file_path):
+    scaler = joblib.load(file_path)
+    return scaler
     
-def test_model(model, X, y):
-    print("\n========= Model Evaluation =========")
+def test_model(model, X, y, is_scaled=False):
+    if is_scaled:
+        scaler = load_scaler('../models/y_scaler.pkl')
+    print("\n=========== Model Evaluation ===========")
     y_pred = model.predict(X)
+    if is_scaled:
+        y_pred = scaler.inverse_transform(y_pred.reshape(-1, 1)).flatten()
+        y = scaler.inverse_transform(y.reshape(-1, 1)).flatten()
     mae = mean_absolute_error(y, y_pred)
     mse = mean_squared_error(y, y_pred)
     r2 = r2_score(y, y_pred)
@@ -29,7 +40,7 @@ def test_model(model, X, y):
     print(f'Mean Absolute Error (MAE): {mae:.4f}')
     print(f'Mean Squared Error (MSE): {mse:.4f}')
     print(f'R^2 Score: {r2:.4f}')
-    print("="*36)
+    print("="*40)
 
 def plot_loss(train_losses, val_losses, title='', save_path=None, log=False):
     plt.figure(figsize=(10, 6))
