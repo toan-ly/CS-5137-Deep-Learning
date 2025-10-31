690241e47a740a780833ab979596b42367f3d42c
toan-ly <toanly0521@gmail.com>
2025-10-31 00:32:13 -0400

    update

--- homework3/src/data/aug.py
+++ homework3/src/data/aug.py
@@ -1,7 +1,8 @@
 from monai.transforms import (
     LoadImageD, EnsureChannelFirstD, Compose, ResizeD,
     ScaleIntensityRangeD, RandFlipD, RandRotateD, RandRotate90D,
-    RandZoomD, EnsureTypeD, AsDiscreteD, RandGaussianNoiseD, LambdaD
+    RandZoomD, EnsureTypeD, AsDiscreteD, RandGaussianNoiseD, 
+    RandCropByPosNegLabelD, LambdaD
 )
 
 from .clahe import apply_clahe
@@ -19,7 +20,17 @@ def get_transforms(
         ResizeD(keys=keys, spatial_size=(im_size, im_size), mode=mode),
         LambdaD(keys=['image'], func=lambda x: x[1:2, ...] if use_green_channel else x),
         LambdaD(keys=['image'], func=lambda x: apply_clahe(x, clip_limit=2.0, tile_grid_size=(8,8), prob=0.5)),
-        # ScaleIntensityRangeD(keys=['image'], a_min=0, a_max=255, b_min=0.0, b_max=1.0, clip=True),
+        ScaleIntensityRangeD(keys=['image'], a_min=0, a_max=255, b_min=0.0, b_max=1.0, clip=True),
+        RandCropByPosNegLabelD(
+            keys=keys,
+            label_key='mask',
+            spatial_size=(256, 256),
+            pos=3,
+            neg=1,
+            num_samples=8,
+            image_key='image',
+            image_threshold=0,
+        ),
     ]
     
     if is_train:
